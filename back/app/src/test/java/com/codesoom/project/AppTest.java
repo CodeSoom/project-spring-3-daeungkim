32ef3add33ade611f47b1cdfed78062bb0f55098
daeungkim <daeungkim93@gmail.com>
2021-10-21 22:48:50 +0900

    :test_tube: 메인 메서드 테스트 작성
    스프링어플리케이션 실행여부 체크하는 테스트 작성

--- back/app/src/test/java/com/codesoom/project/AppTest.java
+++ back/app/src/test/java/com/codesoom/project/AppTest.java
@@ -3,12 +3,57 @@
  */
 package com.codesoom.project;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.mockito.MockedStatic;
+import org.springframework.boot.SpringApplication;
 
-class AppTest {
-    @Test void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+import static org.mockito.Mockito.mockStatic;
+
+
+@DisplayName("App 클래스")
+public final class AppTest {
+    @Nested
+    @DisplayName("main 메서드는")
+    @TestInstance(Lifecycle.PER_CLASS)
+    public final class Describe_main {
+        private MockedStatic<SpringApplication> springMock;
+        private final String[] args = new String[] {};
+
+        @BeforeAll
+        private void beforeAll() {
+            springMock = mockStatic(SpringApplication.class);
+        }
+
+        @BeforeEach
+        private void beforeEach() {
+            springMock
+                .when(() -> SpringApplication.run(App.class, args))
+                .thenReturn(null);
+        }
+
+        @AfterEach
+        private void afterEach() {
+            springMock
+                .verify(() -> SpringApplication.run(App.class, args));
+        }
+
+        @AfterAll
+        private void afterAll() {
+            springMock.close();
+        }
+
+        @Test
+        @DisplayName("스프링 어플리케이션을 실행시킨다.")
+        public void it_executes_spring_application() {
+            App.main(args);
+        }
     }
 }
