f7cbbf62cd094552fecc7ea74aeac7e9d1e51084
daeungkim <daeungkim93@gmail.com>
2021-10-25 15:19:22 +0900

    :memo: 헬스체크 api javadoc 작성
    HealthCheckController의 메서드가 하나뿐이므로
    healthCheck 메서드에 추가적으로 javadoc을 작성하지 않았음

--- back/app/src/main/java/com/codesoom/project/controllers/HealthCheckController.java
+++ back/app/src/main/java/com/codesoom/project/controllers/HealthCheckController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 서버의 정상 작동 여부를 리턴한다.
+ */
 @RestController
 public final class HealthCheckController {
     @RequestMapping("/health")
